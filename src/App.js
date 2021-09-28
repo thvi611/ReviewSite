5c0e77764859ea6a5a96e91e4fad2bf1e20725f5
nanopeniss <trongga9a@gmail.com>
2021-09-28 20:39:09 +0700

    first commit, login screen

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Component/Login';
+import Nav from './Component/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+                <div className="container px-5">
+                    <a className="navbar-brand" href="index.html">404 - Not Found</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">    
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item"><a className="nav-link" href="tintuc.html">Write review</a></li>
+                            <li className="nav-item"><a className="nav-link" href="chitiet.html">Chi tiết</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;

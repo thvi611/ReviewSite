525b1a5be18ce85c45bcb7d61376e137b82172a4
thvi611 <lovivivi611@gmail.com>
2021-10-28 10:50:33 +0700

    AreaDetailItem

--- src/Component/AreaDetail/Item.js
+++ src/Component/AreaDetail/Item.js
@@ -1,13 +1,18 @@
-import styled from "styled-components";
+import React, { Component } from 'react'
 
-export default styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 200px;
-  width: 100%;
-  background-color: grey;
-  color: #fff;
-  margin: 0 15px;
-  font-size: 1em;
-`;
+export default class Item extends Component {
+    render() {
+        return (
+          <div class="card-deck">
+            <div class="card">
+              <img class="card-img-top" src="https://wallpapershome.com/images/pages/pic_h/16681.jpg" alt="Card image cap"></img>
+              <div class="card-body">
+                <a class="card-title text-dark" href="#">Title</a>
+                <p class="card-text">Content</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        )
+    }
+}
